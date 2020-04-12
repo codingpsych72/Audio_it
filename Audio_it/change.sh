8d5fcf5f9dd675d4b553df63de9970ac4ca17323
Mohammed Sharoz <mohammedsharoz72@gmail.com>
2020-04-12 23:47:34 +0530

    Add files via upload

---
+++ Audio_it/change.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+chmod +x audio_convert.py && chmod +x video_convert_start.py && chmod +x audio_convert_start.py && chmod +x script_start.py &&chmod +x youtube_dwn.py &&chmod +x audio_it.sh && chmod +x video_convert.py && chmod +x  youtube_dwn_start.py
+
+
